3924d34358df198f5c5733becd2000a5955a2e68
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2023-10-16 14:46:52 -0300

    Update Projetc

--- ProjetoEscolar.java
+++ ProjetoEscolar.java
@@ -1,11 +1,13 @@
 public class ProjetoEscolar {
     public static void main(String[] args) {
 
-        double nota = 7.0;
+        double nota = 6.0;
 
         if ( nota >= 7.0){
             System.out.println("Aprovado");
-        }else {
+        } else if ( nota >= 5 && nota < 7) {
+            System.out.println("Prova de Recuperação");
+        } else {
             System.out.println("Reprovado");
         }
     }

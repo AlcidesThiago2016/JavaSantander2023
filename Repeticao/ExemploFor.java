dff325339de1886e7a7e0d6e16bb6f7573c6372d
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2023-10-16 16:27:37 -0300

    Update project

---
+++ Repeticao/ExemploFor.java
@@ -0,0 +1,9 @@
+package Repeticao;
+
+public class ExemploFor {
+    public static void main(String[] args) {
+        for ( int contagem = 1; contagem <= 20; contagem++){
+            System.out.println("Contando " + contagem);
+        }
+    }
+}

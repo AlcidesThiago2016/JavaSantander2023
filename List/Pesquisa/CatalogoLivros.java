5b86b9559e9f9f5cbb63e8fbcffe7a0fabb37a63
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2023-10-22 06:14:33 -0300

    Update project

--- List/Pesquisa/CatalogoLivros.java
+++ List/Pesquisa/CatalogoLivros.java
@@ -1,5 +1,7 @@
 package List.Pesquisa;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -50,5 +52,16 @@ public class CatalogoLivros {
             }
         }
         return livroPorTitulo;
+    }
+
+    public static void main(String[] args) {
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 1", 2020);
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 2", 2021);
+        catalogoLivros.adicionarLivro("Livro 2", "Autor 2", 2021);
+        catalogoLivros.adicionarLivro("Livro 3", "Autor 3", 2023);
+        catalogoLivros.adicionarLivro("Livro 4", "Autor 4", 1994);
+
+        System.out.println(catalogoLivros.pesquisarPorAutor("Autor 2"));
     }
 }
